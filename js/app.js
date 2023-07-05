85ab10a58ea6087a61799dec5aef781fa1253e2a
JavierP <zlaattan27@gmail.com>
2023-07-05 17:49:33 -0500

    Modificacion en App.js agregando clases y haciendo dinamico partes del cabecero

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const ingresos = [
-    new Ingreso('salario',2100),
+    new Ingreso('salario',2000),
     new Ingreso('Venta coche',1500)
 ];
 
@@ -12,6 +12,27 @@ function cargarApp(){
     cargarCabecero();
 }
 
+function totalIngresos(){
+    let totalIngreso = 0;
+    for(let ingreso of ingresos){
+        totalIngreso += ingreso.valor;
+    }
+    return totalIngreso;
+}
+
+function totalEgresos(){
+    let totalEgreso = 0;
+    for(let egreso of egresos){
+        totalEgreso += egreso.valor;
+    }
+    return totalEgreso;
+}
+
 function cargarCabecero(){
-    
+    let prespuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgreso = totalEgresos()/totalIngresos();
+    document.getElementById('presupuesto').innerHTML = prespuesto;
+    document.getElementById('porcentaje').innerHTML = porcentajeEgreso;
+    document.getElementById('ingresos').innerHTML = totalIngresos();
+    document.getElementById('egresos').innerHTML = totalEgresos();
 }

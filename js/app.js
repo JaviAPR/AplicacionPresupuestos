f1f560149fa2fc7a9a8dae870a597835069978cb
JavierP <zlaattan27@gmail.com>
2023-07-05 17:40:05 -0500

    Creacion de arreglos de ingresos y egresos, creacion de funciones en app.js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const ingresos = [
+    new Ingreso('salario',2100),
+    new Ingreso('Venta coche',1500)
+];
+
+const egresos = [
+    new Egreso('compra Ropa',1000),
+    new Egreso('Pago de renta',900)
+];
+
+function cargarApp(){
+    cargarCabecero();
+}
+
+function cargarCabecero(){
+    
+}

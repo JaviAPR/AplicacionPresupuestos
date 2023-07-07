8e46bab52d4299297977debc00484da2f51e949b
JavierP <zlaattan27@gmail.com>
2023-07-07 15:22:36 -0500

    Eliminando codigo innecesario

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 const ingresos = [
-    new Ingreso('Sueldo',1000),
-    new Ingreso('Venta coche',1500)
+    
 ];
 
 const egresos = [
-    new Egreso('compra Ropa',1000),
-    new Egreso('Pago de renta',900)
+    
 ];
 
 function cargarApp(){
